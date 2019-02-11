48680691557f9930bbca56162962c81dc8e6dac9
kristiyanyanchev <35624809+kristiyanyanchev@users.noreply.github.com>
2019-02-11 23:50:18 +0200

    Added Repositories and Some of Models

---
+++ Kristiyan_Yanchev_Lorenzo_Eccheli/Models/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    interface IRepository<T>
+    {
+        T GetById(int id);
+        IEnumerable<T> List();
+        IEnumerable<T> List(System.Linq.Expressions.Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Delete(T entity);
+        void Edit(T entity);
+    }
+}
--- Kristiyan_Yanchev_Lorenzo_Eccheli/Models/Repositories/AnnualLeavesRepository.cs
+++ Kristiyan_Yanchev_Lorenzo_Eccheli/Models/Repositories/AnnualLeavesRepository.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Models
+namespace Models.Repositories
 {
-    public class Class1
+    class AnnualLeavesRepository
     {
     }
 }
