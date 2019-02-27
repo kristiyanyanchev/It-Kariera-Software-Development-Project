4b7116ffe307c60ec560a1021234a47577f53f34
lordrager <lorenzoangelov@abv.bg>
2019-02-27 23:22:46 +0200

    concole view

---
+++ Kristiyan_Yanchev_Lorenzo_Eccheli/ConsoleView/IntroductionDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleView
+{
+    public class IntroductionDisplay
+    {
+        public IntroductionDisplay()
+        {
+            Console.WriteLine("           Welcome to e-journal");
+            Console.WriteLine("Choose an option : ");
+            Console.WriteLine("1. Register");
+            Console.WriteLine("2.Log in");
+            Console.WriteLine("3.Exit");
+        }
+
+        private void Command()
+        {
+            int command = int.Parse(Console.ReadLine());
+        }
+    }
+}
--- Kristiyan_Yanchev_Lorenzo_Eccheli/ConsoleView/Program.cs
+++ Kristiyan_Yanchev_Lorenzo_Eccheli/ConsoleView/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleView
     {
         static void Main(string[] args)
         {
-           
+            
         }
     }
 }
