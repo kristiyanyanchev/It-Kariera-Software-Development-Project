dcb7938a685d38961d0a91347af4effe83f73cb6
kristiyanyanchev <35624809+kristiyanyanchev@users.noreply.github.com>
2019-03-05 15:39:22 +0200

    added TeacherViewStudentForm

---
+++ Kristiyan_Yanchev_Lorenzo_Eccheli/Kristiyan_Yanchev_Lorenzo_Eccheli/TeacerViewStudentsForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Kristiyan_Yanchev_Lorenzo_Eccheli
+{
+    partial class TeacerViewStudentsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchFirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.searchLastNameTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.firstNameTextbox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.classTextBox = new System.Windows.Forms.TextBox();
+            this.birthDateTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // searchFirstNameTextBox
+            // 
+            this.searchFirstNameTextBox.Location = new System.Drawing.Point(76, 12);
+            this.searchFirstNameTextBox.Name = "searchFirstNameTextBox";
+            this.searchFirstNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.searchFirstNameTextBox.TabIndex = 0;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(12, 15);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(57, 13);
+            this.firstNameLabel.TabIndex = 1;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(183, 14);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(58, 13);
+            this.lastNameLabel.TabIndex = 2;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // searchLastNameTextBox
+            // 
+            this.searchLastNameTextBox.Location = new System.Drawing.Point(247, 12);
+            this.searchLastNameTextBox.Name = "searchLastNameTextBox";
+            this.searchLastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.searchLastNameTextBox.TabIndex = 3;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(354, 8);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // firstNameTextbox
+            // 
+            this.firstNameTextbox.Location = new System.Drawing.Point(12, 86);
+            this.firstNameTextbox.Name = "firstNameTextbox";
+            this.firstNameTextbox.Size = new System.Drawing.Size(100, 20);
+            this.firstNameTextbox.TabIndex = 5;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(118, 86);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lastNameTextBox.TabIndex = 6;
+            // 
+            // classTextBox
+            // 
+            this.classTextBox.Location = new System.Drawing.Point(224, 86);
+            this.classTextBox.Name = "classTextBox";
+            this.classTextBox.Size = new System.Drawing.Size(36, 20);
+            this.classTextBox.TabIndex = 7;
+            // 
+            // birthDateTextBox
+            // 
+            this.birthDateTextBox.Location = new System.Drawing.Point(12, 112);
+            this.birthDateTextBox.Name = "birthDateTextBox";
+            this.birthDateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.birthDateTextBox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // TeacerViewStudentsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.birthDateTextBox);
+            this.Controls.Add(this.classTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextbox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchLastNameTextBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.searchFirstNameTextBox);
+            this.Name = "TeacerViewStudentsForm";
+            this.Text = "TeacerViewStudentsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchFirstNameTextBox;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox searchLastNameTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox firstNameTextbox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox classTextBox;
+        private System.Windows.Forms.TextBox birthDateTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
