c2a70ba94b2378696fd3330552827b301a7bc756
kristiyanyanchev <35624809+kristiyanyanchev@users.noreply.github.com>
2019-03-07 19:08:20 +0200

    Added Address Validation Class

---
+++ Kristiyan_Yanchev_Lorenzo_Eccheli/Controller/ValidationClasses/Address/DefaultAddressValidator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Controller.ValidationClasses.Address
+{
+    public class DefaultAddressValidator : IAddressValidator
+    {
+        private string RegexPattern { get; set; }
+
+        public DefaultAddressValidator()
+        {
+            RegexPattern = @"[A-Za-z]+[.][_A-Za-z]+[ -_:][0-9]+[_A-za-z]*";
+        }
+
+        public bool Validate(string address)
+        {
+            if (ValidateForbiddenChars(address) && ValidateLength(address) && ValidateWrongFormat(address))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool ValidateForbiddenChars(string address)
+        {
+            if (Regex.Match(address,RegexPattern).Length < address.Length)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool ValidateWrongFormat(string address)
+        {
+            if (Regex.IsMatch(address,RegexPattern))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool ValidateLength(string address)
+        {
+            if (address.Length < 6)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
