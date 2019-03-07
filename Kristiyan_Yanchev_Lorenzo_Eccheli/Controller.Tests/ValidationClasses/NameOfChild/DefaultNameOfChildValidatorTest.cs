0cdb703e1cb3404ac0d552f424dc4b6d09aee485
kristiyanyanchev <35624809+kristiyanyanchev@users.noreply.github.com>
2019-03-07 18:32:32 +0200

    Added Name Of Child Validation Class

---
+++ Kristiyan_Yanchev_Lorenzo_Eccheli/Controller.Tests/ValidationClasses/NameOfChild/DefaultNameOfChildValidatorTest.cs
@@ -0,0 +1,39 @@
+﻿using Controller.ValidationClasses.NameOfChild;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller.Tests.ValidationClasses.NameOfChild
+{
+    [TestFixture]
+    public class DefaultNameOfChildValidatorTest
+    {
+        [TestCase("Ivan2")]
+        public void Validate_ContainsForbiddenChars_ReturnsFalse(string name)
+        {
+            var validator = new DefaultNameOfChildValidator();
+            Assert.False(validator.Validate(name));
+        }
+
+        [TestCase("ivan ivanov")]
+        [TestCase("IvAn Ivanov")]
+        [TestCase("IvanIvanov")]
+        [TestCase("Iv@n Ivanov")]
+        public void Validate_WrongFormat_ReturnsFalse(string name)
+        {
+            var validator = new DefaultNameOfChildValidator();
+            Assert.False(validator.Validate(name));
+        }
+        
+        [TestCase("i i")]
+        [TestCase("")]
+        public void Validate_LengthLessThanRequired(string name)
+        {
+            var validator = new DefaultNameOfChildValidator();
+            Assert.False(validator.Validate(name));
+        }
+    }
+}
---
+++ Kristiyan_Yanchev_Lorenzo_Eccheli/Controller/ValidationClasses/NameOfChild/DefaultNameOfChildValidator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Controller.ValidationClasses.NameOfChild
+{
+   public class DefaultNameOfChildValidator : INameOfChildValidator
+    {
+        private string RegexPattern { get; set; }
+
+        public DefaultNameOfChildValidator()
+        {
+            RegexPattern = @"[A-Z][a-z]+ [A-Z][a-z]+";
+        }
+
+        public bool Validate(string name)
+        {
+            if (ValidateForbiddenChars(name) && ValidateLength(name)&& ValidateFormat(name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool ValidateForbiddenChars(string name)
+        {
+            if (Regex.Match(name,RegexPattern).Length < name.Length)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool ValidateLength(string name)
+        {
+            if (name.Length < 5)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool ValidateFormat(string name)
+        {
+            if (Regex.IsMatch(name,RegexPattern))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
