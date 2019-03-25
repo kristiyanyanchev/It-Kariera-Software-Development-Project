c320e152c607ba99f80d9146a19a40bd2311a1f8
lordrager <lorenzoangelov@abv.bg>
2019-03-25 15:37:21 +0200

    changed language update

---
+++ Kristiyan_Yanchev_Lorenzo_Eccheli/Kristiyan_Yanchev_Lorenzo_Eccheli/RegistrationControls/ParentRegistrationControl.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Kristiyan_Yanchev_Lorenzo_Eccheli.RegistrationControls
+{
+    partial class ParentRegistrationControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ParentRegistrationControl));
+            this.usernameOfChildTextBox = new System.Windows.Forms.TextBox();
+            this.usernameOfChildLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // usernameOfChildTextBox
+            // 
+            resources.ApplyResources(this.usernameOfChildTextBox, "usernameOfChildTextBox");
+            this.usernameOfChildTextBox.BackColor = System.Drawing.Color.FloralWhite;
+            this.usernameOfChildTextBox.Name = "usernameOfChildTextBox";
+            // 
+            // usernameOfChildLabel
+            // 
+            resources.ApplyResources(this.usernameOfChildLabel, "usernameOfChildLabel");
+            this.usernameOfChildLabel.BackColor = System.Drawing.Color.FloralWhite;
+            this.usernameOfChildLabel.Name = "usernameOfChildLabel";
+            // 
+            // ParentRegistrationControl
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Kristiyan_Yanchev_Lorenzo_Eccheli.Properties.Resources.Background;
+            this.Controls.Add(this.usernameOfChildLabel);
+            this.Controls.Add(this.usernameOfChildTextBox);
+            this.Name = "ParentRegistrationControl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox usernameOfChildTextBox;
+        private System.Windows.Forms.Label usernameOfChildLabel;
+    }
+}
---
+++ Kristiyan_Yanchev_Lorenzo_Eccheli/Kristiyan_Yanchev_Lorenzo_Eccheli/RegistrationControls/ParentRegistrationControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kristiyan_Yanchev_Lorenzo_Eccheli.RegistrationControls
+{
+    public partial class ParentRegistrationControl : UserControl
+    {
+        public ParentRegistrationControl()
+        {
+            InitializeComponent();
+        }
+
+        private bool Validate()
+        {
+            if(usernameOfChildTextBox.Text!=null)
+            {
+                //add if text box has numbers
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string NameOfChild
+        {
+            get { return usernameOfChildTextBox.Text; }
+            set
+            {
+                if (Validate()) usernameOfChildTextBox.Text = value;
+                else throw new Exception("Wrong text box");
+            }
+        }
+    }
+}
